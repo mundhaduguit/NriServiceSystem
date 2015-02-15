a33a97b82a57067b7d38684d8dfb00c63b712ea8
vamsi <rvk0106@gmail.com>
2015-02-15 11:10:01 +0530

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,16 @@ ActiveRecord::Schema.define(version: 20150127034940) do
     t.datetime "updated_at"
   end
 
+  create_table "sessions", force: true do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
+
   create_table "users", force: true do |t|
     t.string   "name",                default: "", null: false
     t.string   "login",                            null: false
