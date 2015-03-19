688b345d0673aea94b56b7d56d8abe766f80a014
vamsi <rvk0106@gmail.com>
2015-03-19 07:32:48 +0530

    Pictures

--- app/admin/service.rb
+++ app/admin/service.rb
@@ -4,12 +4,13 @@ form do |f|
   f.input :name
   f.input :description
   f.input :service_type, :as => :select,:collection => ["Paid", "default"],:include_blank => false
-  f.actions do
+ f.file_field :avatar
+  f.actions do 
    f.action :submit, :as => :button
    f.action :cancel, :as => :link
    end
 end
-permit_params :name, :description, :service_type
+permit_params :name, :description, :service_type, :avatar
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
--- db/migrate/20150309110714_create_pictures.rb
+++ db/migrate/20150309110714_create_pictures.rb
@@ -1,9 +1,11 @@
 class CreatePictures < ActiveRecord::Migration
   def change
     create_table :pictures do |t|
-      
+      t.integer :imageable_id
+      t.string :imageable_type
       t.timestamps
     end
     add_attachment :pictures, :avatar
+    add_attachment :services, :avatar
   end
 end
